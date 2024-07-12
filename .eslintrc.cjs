b171d36da58c5e3e92073fddab9506ade2241f3a
Fernando Herrera <fernando.herrera85@gmail.com>
2024-07-12 12:51:06 -0400

    feat: Add IssueView and ListView components

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,13 +6,14 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: [ 'dist', '.eslintrc.cjs' ],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: [ 'react-refresh' ],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    '@typescript-eslint/no-unused-vars': [ 'warn' ]
   },
-}
+};
---
+++ src/GitApp.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router-dom';
+
+export const GitApp = () => {
+  return (
+    <div className="container m-auto max-w-7xl mt-3">
+      <h1>
+        Git Issues <small>Seguimiento de problemas</small>{' '}
+      </h1>
+      <Outlet />
+    </div>
+  );
+};
