6e41d98e472240c3b7af352b766d96a3a91e849e
Fernando Herrera <fernando.herrera85@gmail.com>
2024-07-12 12:58:31 -0400

    Refactor GitApp and index.css for better readability and maintainability

--- src/GitApp.tsx
+++ src/GitApp.tsx
@@ -4,7 +4,7 @@ export const GitApp = () => {
   return (
     <div className="container m-auto max-w-7xl mt-3">
       <h1>
-        Git Issues <small>Seguimiento de problemas</small>{' '}
+        Git Issues <small>Seguimiento de problemas</small>
       </h1>
       <Outlet />
     </div>
--- src/issues/components/IssueList.tsx
+++ src/issues/components/IssueList.tsx
@@ -1,29 +1,21 @@
 import { IssueItem } from './IssueItem';
 
 export const IssueList = () => {
-    return (
-        <div className="card border-white">
-            <div className="card-header bg-dark">
-                <ul className="nav nav-pills card-header-pills">
-                    <li className="nav-item">
-                        <a className="nav-link active">All</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link">Open</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link">Closed</a>
-                    </li>
-                </ul>
-            </div>
-            <div className="card-body text-dark">
-                {
-                    [1,2,3].map( issue => (
-                        <IssueItem key={issue} />
-                    ))
-                
-                }                
-            </div>
-        </div>
-    )
-}
+  return (
+    <>
+      {/* Botones de All, Open, Closed */}
+      <div className="flex gap-4">
+        <button className="btn active">All</button>
+        <button className="btn">Open</button>
+        <button className="btn">Closed</button>
+      </div>
+
+      {/* Lista de issues */}
+      <div className="mt-4">
+        {[1, 2, 3].map((issue) => (
+          <IssueItem key={issue} />
+        ))}
+      </div>
+    </>
+  );
+};
