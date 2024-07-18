375c282a4d8c189d69df9c153f10cb68f20c440d
Fernando Herrera <fernando.herrera85@gmail.com>
2024-07-18 14:50:21 -0400

    chore: Update npm dependencies and add React Query and Devtools

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,19 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import './index.css';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </React.StrictMode>
 );
