274600093003e1124b8af6d64e264c369367f3b0
Fernando Herrera <fernando.herrera85@gmail.com>
2024-07-19 12:48:47 -0400

    chore: Update useLabels hook to include initialData for labels query

--- src/issues/hooks/useLabels.tsx
+++ src/issues/hooks/useLabels.tsx
@@ -1,11 +1,52 @@
 import { useQuery } from '@tanstack/react-query';
 import { getLabels } from '../actions';
+import { GithubLabel } from '../interfaces';
 
 export const useLabels = () => {
   const labelsQuery = useQuery({
     queryKey: ['labels'],
     queryFn: getLabels,
     staleTime: 1000 * 60 * 60, // 1 hora de stale time
+
+    // placeholderData: [
+    //   {
+    //     id: 739777675,
+    //     node_id: 'MDU6TGFiZWw3Mzk3Nzc2NzU=',
+    //     url: 'https://api.github.com/repos/facebook/react/labels/Component:%20Component%20API',
+    //     name: 'Component: Component API',
+    //     color: 'd4c5f9',
+    //     default: false,
+    //   } satisfies GithubLabel,
+
+    //   {
+    //     id: 139734344,
+    //     node_id: 'MDU6TGFiZWwxMzk3MzQzNDQ=',
+    //     url: 'https://api.github.com/repos/facebook/react/labels/Component:%20Test%20Utils',
+    //     name: 'Component: Test Utils',
+    //     color: 'eb6420',
+    //     default: false,
+    //   } satisfies GithubLabel,
+    // ],
+
+    initialData: [
+      {
+        id: 739777675,
+        node_id: 'MDU6TGFiZWw3Mzk3Nzc2NzU=',
+        url: 'https://api.github.com/repos/facebook/react/labels/Component:%20Component%20API',
+        name: 'Component: Component API',
+        color: 'd4c5f9',
+        default: false,
+      } satisfies GithubLabel,
+
+      {
+        id: 139734344,
+        node_id: 'MDU6TGFiZWwxMzk3MzQzNDQ=',
+        url: 'https://api.github.com/repos/facebook/react/labels/Component:%20Test%20Utils',
+        name: 'Component: Test Utils',
+        color: 'eb6420',
+        default: false,
+      } satisfies GithubLabel,
+    ],
   });
 
   return {
